9fa92511e41f81774471da3aa92f91feaed79fa3
levion8304 <ngocliem10a5nth@gmail.com>
2024-10-31 22:42:06 +0700

    Hoàn thành trang giới thiệu bên Client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,10 @@
 const homeRoutes = require("./home.route");
 const userRoutes = require("./user.route");
+const aboutRoutes = require("./about.route");
 
 module.exports = (app) => {
   app.use("/", homeRoutes);
   app.use("/user", userRoutes);
+  app.use("/about", aboutRoutes);
 }
 
